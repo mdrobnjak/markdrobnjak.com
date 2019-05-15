8f56600424d1e6e74cf6efe9dc81e26881e30e1c
mdrobnjak <42944601+mdrobnjak@users.noreply.github.com>
2019-05-15 13:29:45 -0400

    Cosmetic cleanup, added crate.png for future texture mapping.

--- art.js
+++ art.js
@@ -45,7 +45,7 @@ var Init = function () {
 
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
-	gl.viewport(0,0,window.innerWidth,window.innerHeight);
+	gl.viewport(0, 0, window.innerWidth, window.innerHeight);
 	
 	gl.clearColor(0.75, 0.85, 0.8, 1.0);
 	gl.enable(gl.DEPTH_TEST);
